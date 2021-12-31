cbd7191e17ab36e7f1389496468988b84f85d385
zhixiong <lxsbw@126.com>
2021-12-31 17:22:35 +0800

    整理合成一个代码文件

---
+++ src/example/thenByDescending.ts
@@ -0,0 +1,28 @@
+let orderByID, persons, thenByAge, thenByName;
+
+import List from '../linq';
+
+interface Person {
+  ID: number;
+  Age: number;
+  Name: string;
+}
+
+persons = [
+  { ID: 0, Age: 30, Name: 'A' },
+  { ID: 1, Age: 25, Name: 'B' },
+  { ID: 2, Age: 2, Name: 'G' },
+  { ID: 2, Age: 18, Name: 'C' },
+  { ID: 1, Age: 30, Name: 'D' },
+  { ID: 1, Age: 25, Name: 'E' } ,
+  { ID: 2, Age: 15, Name: 'F' }
+];
+
+orderByID = new List<Person>(persons).OrderByDescending(x => x.ID).ToArray();
+thenByAge = new List<Person>(persons).OrderByDescending(x => x.ID).ThenBy(x => x.Age).ToArray();
+thenByName = new List<Person>(persons).OrderByDescending(x => x.ID).ThenBy(x => x.Age).ThenByDescending(x => x.Name).ToArray();
+
+console.log('orderByID:', orderByID);
+console.log('thenByAge:', thenByAge);
+console.log('thenByName:', thenByName);
+// console.log('persons:', persons);
