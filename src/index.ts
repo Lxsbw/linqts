5abd08cbf13c41da00d4018bfb1dc17d45e02dae
zhixiong <lxsbw@126.com>
2022-01-07 09:09:56 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,5 @@
  *
  * Documentation from Linq .NET specification (https://msdn.microsoft.com/en-us/library/system.linq.enumerable.aspx)
  *
- * Created by Flavio Corpa (@kutyel)
- * Copyright © 2016 Flavio Corpa. All rights reserved.
- *
  */
 export { default as Linq } from './linq';
