c9d36c62e482a66a1b28058ab3d046e33141b279
zhixiong <lxsbw@outlook.com>
2023-06-11 23:38:11 +0800

    clone & equal update

---
+++ test/equal.ts
@@ -0,0 +1,28 @@
+import Linq from '../src/linq';
+
+const obj1 = { A: '1', B: '2', C: '3'};
+const obj2 = { A: '1', B: '2', C: '3', D: '4'};
+const obj3 = { A: '1', B: '2', C: '3', dt: new Date('2018-02-03') };
+const obj4 = { A: '1', B: '2', C: '3', dt: new Date('2018-02-03') };
+console.log(new Linq().equals(obj1, obj2));
+console.log();
+console.log(new Linq().equals(new Date(), obj1));
+console.log();
+console.log(new Linq().equals({}, {}));
+console.log();
+console.log(new Linq().equals({}, {arr:'a'}));
+console.log();
+console.log(new Linq().equals({}, {arr:[]}));
+console.log();
+console.log(new Linq().equals(new Date('2018-02-03'), new Date('2018-02-04')));
+console.log();
+console.log(new Linq().equals(new Date('2018-02-03'), new Date('2018-02-03')));
+console.log();
+console.log(new Linq().equals([1], [1, 2]));
+console.log();
+console.log(new Linq().equals([1], [1]));
+console.log();
+console.log(new Linq().equals(new RegExp('abc', 'g'), new RegExp('abcd', 'g')));
+console.log();
+console.log(new Linq().equals(obj3, obj4));
+console.log();
